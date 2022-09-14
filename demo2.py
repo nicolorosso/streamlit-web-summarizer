589ad68018a17b5e7170444e89d9e4a272ef4dca
nicolorosso <98038058+nicolorosso@users.noreply.github.com>
2022-09-14 18:21:24 +0200

    Update demo2.py

--- demo2.py
+++ demo2.py
@@ -6,8 +6,6 @@ auth_key = "9d5d6377-86f6-5862-90e2-783b691526a2:fx"
 translator = deepl.Translator(auth_key)
 
 from summarizer import Summarizer
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM 
-#from transformers import pipeline
 
 
 
@@ -24,12 +22,27 @@ def translate(text):
 def translate2(text):
   new_text = translator.translate_text(text, target_lang="EN-GB")
   body = new_text.text
-  tokenizer = AutoTokenizer.from_pretrained("facebook/bart-large-cnn")
-  model = AutoModelForSeq2SeqLM.from_pretrained("facebook/bart-large-cnn")
-  tokens_input = tokenizer.encode("summarize: "+body, return_tensors='pt', max_length=1024, truncation=True)
-  ids = model.generate(tokens_input, min_length=150, max_length=180)
-  summary = tokenizer.decode(ids[0], skip_special_tokens=True)
-  final_text = translator.translate_text(summary, target_lang="IT")
+  model = Summarizer('distilbert-base-uncased', hidden=[-1,-2], hidden_concat=True)
+  result = model(body, num_sentences=4)
+  final_text = translator.translate_text(result, target_lang="IT")
+  return final_text.text
+
+@st.cache #decorator
+def translate3(text):
+  new_text = translator.translate_text(text, target_lang="EN-GB")
+  body = new_text.text
+  model = Summarizer('distilbert-base-uncased', hidden=[-1,-2], hidden_concat=True)
+  result = model(body, num_sentences=5)
+  final_text = translator.translate_text(result, target_lang="IT")
+  return final_text.text
+
+@st.cache #decorator
+def translate4(text):
+  new_text = translator.translate_text(text, target_lang="EN-GB")
+  body = new_text.text
+  model = Summarizer('distilbert-base-uncased', hidden=[-1,-2], hidden_concat=True)
+  result = model(body, num_sentences=6)
+  final_text = translator.translate_text(result, target_lang="IT")
   return final_text.text
 
 
@@ -49,29 +62,29 @@ def main():
 		st.subheader("Sto lavorando anche con altri modelli, per il momento condivido questa demo con BERT ")
 
 		message = st.text_area("Enter Text","Type Here....")
-		summary_options = st.selectbox("Choose Summarizer",['bert','Bart-facebook'])
+		summary_options = st.selectbox("Choose Summarizer",['bert n.periodi:3','bert n.periodi:4', 'bert n.periodi:5', 'bert n.periodi:6', 'bert n.periodi:2'])
 		if st.button("Summarize"):
-			if summary_options == 'bert':
-				st.text("Using Bert Summarizer ..")
+			if summary_options == 'bert n.periodi:3':
+				st.text("Using bert n.periodi:3 ..")
 				summary_result = translate(message)
 				
 				
-			elif summary_options == 'Bart-facebook':
-				st.text("Using Bart-facebook Summarizer ..")
+			elif summary_options == 'bert n.periodi:4':
+				st.text("Using bert n.periodi:4 ..")
 				summary_result = translate2(message)
 			
-			#elif summary_options == 'pipeline api':
-				#st.text("Using Pipeline API Summarizer ..")
-				#summary_result = translate3(message)
+			elif summary_options == 'bert n.periodi:5':
+				st.text("Using bert n.periodi:5 ..")
+				summary_result = translate3(message)
 				
-			#elif summary_options == 't-5 large':
-				#st.text("Using t-5 Summarizer ..")
-				#summary_result = translate4(message)
+			elif summary_options == 'bert n.periodi:6':
+				st.text("Using bert n.periodi:6 ..")
+				summary_result = translate4(message)
 				
 			else:
 				st.warning("Using Default Summarizer")
 				st.text("Using Bert Summarizer ..")
-				summary_result = translate2(message)
+				summary_result = translate(message)
 			st.success(summary_result)
 
 if __name__ == '__main__':
